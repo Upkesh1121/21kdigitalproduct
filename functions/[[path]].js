7cd1c14bc2c53c1bfbda36146f67766bcd237490
Upkesh1121 <upkeshpal47@gmail.com>
2026-06-12 17:07:18 +0530

    Route API requests before SSR fallback

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,5 +1,19 @@
 ﻿import server from '../dist/server/server.js'
+import { onRequest as apiRequest } from './api/[[path]].js'
 
 export async function onRequest(context) {
+  const url = new URL(context.request.url)
+
+  if (url.pathname === '/api' || url.pathname.startsWith('/api/')) {
+    const apiPath = url.pathname.replace(/^\/api\/?/, '')
+    return apiRequest({
+      ...context,
+      params: {
+        ...context.params,
+        path: apiPath,
+      },
+    })
+  }
+
   return server.fetch(context.request, context.env, context)
 }
